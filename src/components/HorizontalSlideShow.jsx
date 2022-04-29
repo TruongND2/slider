c4df88667757cbf3198d24ea0282ae589721a551
truongnd2 <truongnd2@rikkeisoft.com>
2022-04-29 14:56:52 +0700

    Add responsive

--- src/components/HorizontalSlideShow.jsx
+++ src/components/HorizontalSlideShow.jsx
@@ -16,25 +16,10 @@ export const HorizontalSlideShow = ({ pictures }) => {
     beforeChange: (old, next) => setCurrentSlide(next),
     responsive: [
       {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
         breakpoint: 480,
         settings: {
+          className: "center w-[300px]",
+          centerPadding: "0px",
           slidesToShow: 1,
           slidesToScroll: 1,
         },
